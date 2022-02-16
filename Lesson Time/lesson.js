cc96cf3f24c294f675cc5bc38ab093f0a597a6b5
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-16 19:21:35 +0400

    Dəyişənlər və onların tipləri Dərs1

---
+++ Lesson Time/lesson.js
@@ -0,0 +1,27 @@
+"use strict"
+let name = undefined;
+name = "Qurban"
+console.log(name);
+
+let myBirthday = "menim ad gunum"
+const b = `${myBirthday}`
+console.log(b);
+
+let name1 = "Qurban";
+let age = 1;
+console.log(name1 / age);
+
+let muqayise = 4 < 5
+console.log(muqayise)
+
+let muqayise1;
+console.log(muqayise1)
+
+let name2;
+console.log(typeof (name2))
+
+let name3;
+window.prompt(name3)
+
+let name4;
+window.confirm(name4)
