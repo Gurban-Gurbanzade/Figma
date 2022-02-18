3c95d26bf10ceed36cf6172a384c01f0b32186d9
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-18 13:14:39 +0400

    JS tapşırıqlar üçün yeni qovluq yaradıldı və verilənlər əlavə olundu

---
+++ JS Task/T-1 Array/script.js
@@ -0,0 +1,44 @@
+// const istifadəçi = [{
+//         adi: "Nail";
+//         soyadi: "Mammadov";
+//         yasi: "24";
+//         cinsi: "kisi";
+//         milliyet: "azerbaycanli"
+//     } {
+//         adi: "Ahmed";
+//         soyadi: "Huseynzadeh";
+//         yasi: "54";
+//         cinsi: "kisi";
+//         milliyet: "azerbaycanli"
+//     } {
+//         adi: "Leylai";
+//         soyadi: "Mustafa";
+//         yasi: "17";
+//         cinsi: "qadin";
+//         milliyet: "arabic"
+//     } {
+//         adi: "Hatem";
+//         soyadi: "Yusif";
+//         yasi: "38";
+//         cinsi: "kisi";
+//         milliyet: "fars"
+//     } {
+//         adi: "Ayse";
+//         soyadi: "Korkmaz";
+//         yasi: "10";
+//         cinsi: "qadin";
+//         milliyet: "turk"
+//     }
+
+// ]
+
+const ad = "Nail";
+const age = [25, 26, 8, 10];
+if (age === 25) {
+    console.log('Ahmed')
+}
+// Butun istifadeciler arasinda 24 yasdan kicikleri secin
+
+// Butun istifadecilerin yaslarinin cemini tapin
+
+// Qadin istifadecileri ayri bir array seklinde verin
