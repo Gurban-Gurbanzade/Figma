4fde5eb26525f859a75fac5aa1b8cf57c9636699
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-19 23:59:28 +0400

    Function öyrənməyə çalışdıq

--- Lesson Time/lesson.js
+++ Lesson Time/lesson.js
@@ -1,31 +1,17 @@
-"use strict"
-
-// let azn = "100";
-// let dollar = "200";
-
-// console.log(+azn + +dollar);
-
-// alert(+" ");
-
-let a = 3;
-let b = 2;
-console.log(a % b);
-console.log(a ** b);
-
-let numb = "0";
-console.log("0" == false);
-
-let numb1 = " ";
-console.log(" " == undefined);
-
-let name1 = "a";
-let name2 = "z";
-console.log(name1 < name2);
-
-let name3 = 3;
-let name4 = 4;
-if (name3 > name4) {
-    console.log("dogru")
-} else {
-    console.log("sehv")
+function name1(x, y, z) {
+    if (z == 3) {
+        console.log("xos gelmisiniz")
+    } else if (y == 2) {
+        console.log("giris qadagandir")
+    }
 }
+name1(2, 3);
+
+
+
+// let age = prompt();
+// if (age > 19) {
+//     console.log("yasi coxdur")
+// } else {
+//     console.log("yasi azdir")
+// }
