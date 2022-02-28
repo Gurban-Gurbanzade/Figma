52fdc37699800ee5471100fa2ec0e1d7aa478974
Gurban-Gurbanzade <98018715+Gurban-Gurbanzade@users.noreply.github.com>
2022-02-28 19:32:31 +0400

    if tasks

--- Lesson Time/lesson.js
+++ Lesson Time/lesson.js
@@ -1,10 +1,31 @@
-let saheKvadrat = () => {
-  let a = prompt("Eni", "");
-  let b = prompt("Uzunlugu", "");
-  let c = "a";
-  if (a.match(c) || b.match(c)) {
-    console.log("Reqem daxil edin");
-  } else {
-    console.log(a * b);
-  }
-};
+// for (let i = 0; i < 3; i++) {
+//   alert(i);
+// }
+// for (; i < 3; i++) {
+//   alert(i);
+// }
+// let cem = 0;
+// while (true) {
+//   let deyer = +prompt("reqem yazin", "");
+//   if (deyer) break;
+//   cem += deyer;
+// }
+// alert("cemi:" + cem);
+// for (let i = 0; i < 10; i++) {
+//   if (i % 2 == 0) continue;
+//   alert(i);
+// }
+// dovretme: for (let i = 0; i < 3; i++) {
+//   for (let j = 0; j < 3; j++) {
+//     let girish = prompt(`kordinatdaki deyer deyer(${i},${j})`, "");
+//     if (!girish) break dovretme;
+//   }
+// }
+// let i = 0;
+// while (i++ < 5) alert(i);
+// let i = 0;
+// while (i++ < 3) alert(i);
+// let i = prompt("deyer yazin", "");
+// while (i < 50) {
+//   i = prompt("deyer yazin", "");
+// }
