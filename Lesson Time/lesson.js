f1d511a01057deedcf9e3819bbf5ba3e420161a7
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-12 19:35:04 +0400

    Arraylar

--- Lesson Time/lesson.js
+++ Lesson Time/lesson.js
@@ -1,15 +1,31 @@
-// let obj={};
-// function A(){...};
-// function B(){...};
+// function sunInput() {
+//   let reqemler;
+//   while (true) {
+//     let deyer = +prompt("reqem yazan", 0);
+//     if (deyer === "" || deyer === null || !isFinite(deyer)) break;
 
-// alert(new A()==nev B());
-
-// let str = "bir kere yukselen bayraq bir daha enmez";
-
-// if (~str.lastIndexof("bir")) {
-//   alert("tapdim");
+//     reqemler.push(+deyer);
+//   }
+//   let cem = 0;
+//   for (let reqem of reqemler) {
+//     cem += reqem;
+//   }
+//   return cem;
 // }
+// alert(sunInput());
 
-// alert("yuzdeyuz".includes("uz"));
+// let arr = ["eve", "getmek", "isteyirem", "ancaq", "hava"];
+// arr.splice(-1, 0, 3, 4);
+// alert(arr);
+// let str = "test";
+// let arr = ["t", "e", "s", "t"];
+// alert;
 
-alert(str.slice(-4, -1));
+// let arr = [1, 2];
+// let arrayLike = {
+//   0: something,
+// };
+
+// [1,-2,15,2,0,8].sort(function(a,b)){
+//     alert(a+"<>"+b);
+// }
