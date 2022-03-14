39ce1226affcb5321d6b57210174dce2671b9181
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-14 19:27:55 +0400

    map haqqında çalışmalar

--- Lesson Time/lesson.js
+++ Lesson Time/lesson.js
@@ -1,31 +1,58 @@
-// function sunInput() {
-//   let reqemler;
-//   while (true) {
-//     let deyer = +prompt("reqem yazan", 0);
-//     if (deyer === "" || deyer === null || !isFinite(deyer)) break;
-
-//     reqemler.push(+deyer);
-//   }
-//   let cem = 0;
-//   for (let reqem of reqemler) {
-//     cem += reqem;
-//   }
-//   return cem;
-// }
-// alert(sunInput());
-
-// let arr = ["eve", "getmek", "isteyirem", "ancaq", "hava"];
-// arr.splice(-1, 0, 3, 4);
-// alert(arr);
-// let str = "test";
-// let arr = ["t", "e", "s", "t"];
-// alert;
-
-// let arr = [1, 2];
-// let arrayLike = {
-//   0: something,
+//
+// let araliq = {
+//   start: 1,
+//   end: 5,
 // };
 
-// [1,-2,15,2,0,8].sort(function(a,b)){
-//     alert(a+"<>"+b);
+// araliq[Symbol.interator] = function () {
+//   return {
+//     current: this.start,
+//     last: this.end,
+//     next() {
+//       if (this.current <= this.last) {
+//         return { done: false, value: this.current++ };
+//       } else {
+//         return { done: true };
+//       }
+//     },
+//   };
+// };
+// for (let num of araliq) {
+//   alert(num);
 // }
+
+// let map = new Map();
+
+// map.set("1", "str1");
+
+// alert(map.get("1"));
+
+// alert(map.size);
+
+// let map = new Map(
+//   Object.entries({
+//     name: nail,
+//     age: 26,
+//   })
+// );
+
+// let yeniMap = newMap;
+// let set= new Set{};
+
+// let nail ={name,"nail"};
+// let nail ={name,"elmar"};
+// let nail ={name,"elvin"};
+
+// set.add{nail};
+// set.add{elmar};
+// set.add{mary};
+// alert{set.size}
+// let set=new Set{["portagal","alma","heyva"]}
+
+let nail = { name: "Nail" };
+let map = new Map();
+map.set(nail, "...");
+nail = null;
+map.keys(nail);
+
+console.log(map);
