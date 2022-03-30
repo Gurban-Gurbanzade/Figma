4564eaca50e9f58c9a0d341e189346ae9d40607b
gurbangurbanzade <98018715+gurbangurbanzade@users.noreply.github.com>
2022-03-30 23:41:22 +0400

    30.03 tarixində dərsdə yazılan kodlar

--- Lesson Time/lesson.js
+++ Lesson Time/lesson.js
@@ -1,58 +1,9 @@
-//
-// let araliq = {
-//   start: 1,
-//   end: 5,
-// };
+let student = {
+  name: "Nail",
+  age: 26,
+  adminMi: false,
+  lesson: ["html", "css", "js"],
+  compare: null,
+};
 
-// araliq[Symbol.interator] = function () {
-//   return {
-//     current: this.start,
-//     last: this.end,
-//     next() {
-//       if (this.current <= this.last) {
-//         return { done: false, value: this.current++ };
-//       } else {
-//         return { done: true };
-//       }
-//     },
-//   };
-// };
-// for (let num of araliq) {
-//   alert(num);
-// }
-
-// let map = new Map();
-
-// map.set("1", "str1");
-
-// alert(map.get("1"));
-
-// alert(map.size);
-
-// let map = new Map(
-//   Object.entries({
-//     name: nail,
-//     age: 26,
-//   })
-// );
-
-// let yeniMap = newMap;
-// let set= new Set{};
-
-// let nail ={name,"nail"};
-// let nail ={name,"elmar"};
-// let nail ={name,"elvin"};
-
-// set.add{nail};
-// set.add{elmar};
-// set.add{mary};
-// alert{set.size}
-// let set=new Set{["portagal","alma","heyva"]}
-
-let nail = { name: "Nail" };
-let map = new Map();
-map.set(nail, "...");
-nail = null;
-map.keys(nail);
-
-console.log(map);
+console.log(student.lesson[0]);
